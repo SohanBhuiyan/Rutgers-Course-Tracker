da72d4e233a21eb3a8df566b0f7f36edacf620db
Sohan Bhuiyan <sohanura@gmail.com>
2019-08-31 11:00:52 -0400

    Remove unnecessary comments
    * Remove old Course initialization which was commented out
    * Change some comment wording to be more clear

--- src/MainFile/MainController.java
+++ src/MainFile/MainController.java
@@ -24,25 +24,18 @@ public class MainController implements Initializable {
     @FXML PasswordField tf_password;
     @FXML ComboBox<String> semester;
     @FXML TextField academicYear;
-    ObservableList<Course> items = FXCollections.observableArrayList();
-    ObservableList<String> semesterOptions = FXCollections.observableArrayList("FALL","SPRING");
+    private ObservableList<Course> items = FXCollections.observableArrayList();
+    private ObservableList<String> semesterOptions = FXCollections.observableArrayList("FALL","SPRING");
 
 
 
     /** code is attached to the snipe button. Sets up variables from textfield that user fills out.
-     * also updates the TableView content so it shows it correctly.
-     * @param event
-     */
-
-    // Snipe Button Action
+     * also updates the TableView content so it shows it correctly.*/
     public void addNewCourseToTracker(ActionEvent event) {
         String semesterSelected;
-       // setUpColumns();
         semesterSelected = semester.getValue();
         String name_of_courseSection = courseUrl.getText();
         String name_of_course = courseName.getText();
-        //old way
-        //Course course = new Course(username,password,this,name_of_courseSection,name_of_course);
 
         Course course = new Course.Builder()
                 .username(tf_username.getText())
@@ -52,16 +45,11 @@ public class MainController implements Initializable {
                 .controller(this)
                 .status("sniping....")
                 .build();
-
-        //items.add(course);
-      // tableView.setItems(items);
         tableView.getItems().add(course);
-      //  StartUp.tableView.getItems().add(course);
 
         semesterSelected = semesterSelected.equals("FALL") ? "9" : "1";
         String year = academicYear.getText();
-        // this url will take chromium to where only the course provided is available, making it easier to determine if its open or not.
-        String courseUrl = "http://sis.rutgers.edu/soc/#keyword?keyword="+name_of_courseSection+"&semester="+semesterSelected+year+"&campus=NB&level=U";
+        String courseUrl = "http://sis.rutgers.edu/soc/#keyword?keyword="+name_of_courseSection+"&semester="+semesterSelected+year+"&campus=NB&level=U";  //url takes you directly to the course page
         course.creatingTracker(courseUrl);
     }
 
@@ -71,7 +59,7 @@ public class MainController implements Initializable {
         courseNameColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("courseName"));
         statusColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("status"));
     }
-    // called when a class has been successfully registered.
+    // refresh the listview to show new added courses
     public void updateListView() {
         tableView.refresh();
         System.out.println("Done updating");
@@ -81,7 +69,6 @@ public class MainController implements Initializable {
         sectionColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("section"));
         courseNameColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("courseName"));
         statusColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("status"));
-        //sectionColumn.setCellValueFactory(new PropertyValueFactory<Course,String>("section"));
         tableView.setItems(items);
         semester.setItems(semesterOptions);
 
